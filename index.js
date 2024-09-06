193c43e44efbfea7baddae9f836dcfc594bf93c3
Iulian Nichifor <73722884+Seven719@users.noreply.github.com>
2024-09-06 09:52:25 +0100

    Added functionality using js for going through the images
    Added data attributes in html

---
+++ index.js
@@ -0,0 +1,22 @@
+const buttons = document.querySelectorAll("[data-carousel-button]");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1;
+    const slides = document.querySelector("[data-carousel]");
+    const dots = document.querySelector("[data-dots]");
+
+    const activeSlide = slides.querySelector("[data-active]");
+    const activeDot = dots.querySelector("[data-active]");
+
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+    if (newIndex < 0) newIndex = slides.children.length - 1;
+    if (newIndex >= slides.children.length) newIndex = 0;
+
+    slides.children[newIndex].dataset.active = true;
+    delete activeSlide.dataset.active;
+
+    dots.children[newIndex].dataset.active = true;
+    delete activeDot.dataset.active;
+  });
+});
